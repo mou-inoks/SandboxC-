32f9f4fbb43bc438039d57cbcaa15a54597f5204
mou-inoks <mou@inokscapital.com>
2022-10-20 10:58:23 +0200

    added stuff

---
+++ Sandbox/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+//CodeWar Multiples of 3 or 5 =====================================================
+
+Console.WriteLine(SolutionComplicated(10));
+
+static int Solution(int value)
+{
+    var finalNumber = 0;
+
+    for (int i = 0; i < value; i++)
+    {
+        Console.WriteLine("current number : " + i);
+        Console.WriteLine("final number : " + finalNumber);
+        Console.WriteLine("_______________________________");
+        if ((i % 3 == 0) || (i % 5 == 0))
+            finalNumber = finalNumber + i;
+    };
+
+
+    return finalNumber;
+}
+
+static int SolutionComplicated(int value)
+{
+    return Enumerable.Range(0, value).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+}
+
+// ===============================================================================
